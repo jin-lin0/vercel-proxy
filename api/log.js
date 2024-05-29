645b53755f569aec0f1f30b09e0dd6c7ddf47b0d
hejinlin <769316680@qq.com>
2024-05-29 18:30:57 +0800

    feat: get external IP

--- api/log.js
+++ api/log.js
@@ -10,15 +10,22 @@ export default async (req, res) => {
     const db = client.db("vercel");
     const collection = db.collection("log");
 
-    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    // const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
     const { method, url } = req;
     const userAgent = req.headers["user-agent"];
     const ipinfoToken = process.env.IPINFO_TOKEN;
-    const geoInfo = await axios.get(`https://ipinfo.io/${ip}/geo`, {
+    const params = {
       headers: { Authorization: `Bearer ${ipinfoToken}` },
-    });
+    };
+    const externalIP = await axios.get(
+      "https://api.ipify.org?format=json",
+      params
+    );
+    const ip = externalIP.data.ip;
+    const geoInfo = await axios.get(`https://ipinfo.io/${ip}/geo`, params);
 
     console.log(geoInfo.data);
+    console.log(ip);
 
     const logEntry = {
       ip,
